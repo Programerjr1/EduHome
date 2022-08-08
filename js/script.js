29adc2bb157e3bcb2f84093fade6228c09bf891d
Programer Jr <yayoub2020@gmail.com>
2022-08-08 14:47:09 +0100

    EduHome Project

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const toggleElem = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < navTogglers.length; i++) {
+  navTogglers[i].addEventListener("click", function () {
+    toggleElem(navbar);
+    toggleElem(overlay);
+  });
+}
+
+
+const header = document.querySelector("[data-header]");
+// const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    // backTopBtn.classList.add("active");
+    header.classList.add("header-anim");
+  } else {
+    header.classList.remove("active");
+    // backTopBtn.classList.remove("active");
+    header.classList.remove("header-anim");
+  }
+});
+
+const whishlistBtns = document.querySelectorAll("[data-wish-btn]");
+
+for (let i = 0; i < whishlistBtns.length; i++) {
+  whishlistBtns[i].addEventListener("click", function () {
+    toggleElem(this);
+  });
+}
